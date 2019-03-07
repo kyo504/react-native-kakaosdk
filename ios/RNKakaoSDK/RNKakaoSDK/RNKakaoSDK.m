b72b32333866bd3bd72d3f7a2cb6328ff2631795
Daniel Jung <daniel.jung@radishfiction.com>
2019-03-07 22:54:22 +0900

    Add ios project

---
+++ ios/RNKakaoSDK/RNKakaoSDK/RNKakaoSDK.m
@@ -0,0 +1,13 @@
+//
+//  RNKakaoSDK.m
+//  RNKakaoSDK
+//
+//  Created by Daniel Jung on 07/03/2019.
+//  Copyright © 2019 Daniel Jung. All rights reserved.
+//
+
+#import "RNKakaoSDK.h"
+
+@implementation RNKakaoSDK
+
+@end
